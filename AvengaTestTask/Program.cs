45c8c530d40fb854cf992be73618384237c4179c
Alex <proskuraaleksandr93@gmail.com>
2020-07-29 14:34:21 +0300

    Get popular pizza configs and print info

--- AvengaTestTask/Program.cs
+++ AvengaTestTask/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using AvengaTestTask.Models;
 using Newtonsoft.Json;
@@ -13,7 +14,11 @@ namespace AvengaTestTask
         {
             var pizzas = GetData();
 
-            Console.WriteLine("Hello World!");
+            var mostPopularPizzas = GetPopularPizzas(pizzas);
+
+            PrintPizzaInfo(mostPopularPizzas);
+
+            Console.ReadKey();
         }
 
         private static IList<Pizza> GetData()
@@ -27,6 +32,46 @@ namespace AvengaTestTask
             {
                 JsonSerializer serializer = new JsonSerializer();
                 return serializer.Deserialize<IList<Pizza>>(jsonReader);
+            }
+        }
+
+        private static Dictionary<Pizza, int> GetPopularPizzas(IList<Pizza> pizzas)
+        {
+            var pizzaConfigs = new Dictionary<Pizza, int>();
+
+            foreach (var pizza in pizzas)
+            {
+                var exists = pizzaConfigs.Keys.Where(up => up.Equals(pizza))?.FirstOrDefault();
+                if (exists != null)
+                {
+                    pizzaConfigs[exists] += 1;
+                }
+                else
+                {
+                    pizzaConfigs.Add(pizza, 1);
+                }
+            }
+
+            var sortedDictionary = (from entry in pizzaConfigs orderby entry.Value descending select entry)
+                .Take(20)
+                .ToDictionary(pair => pair.Key, pair => pair.Value);
+            return sortedDictionary;
+        }
+
+        private static void PrintPizzaInfo(Dictionary<Pizza, int> mostPopularPizzas)
+        {
+            var i = 1;
+            foreach (var pizzaConfig in mostPopularPizzas)
+            {
+
+                Console.WriteLine($"- Pizza configuration {i}, was ordered {pizzaConfig.Value} times -");
+                Console.WriteLine("Toppings:");
+                foreach (var topping in pizzaConfig.Key.Toppings)
+                {
+                    Console.WriteLine($"{topping}");
+                }
+                Console.WriteLine("\n");
+                i++;
             }
         }
     }
