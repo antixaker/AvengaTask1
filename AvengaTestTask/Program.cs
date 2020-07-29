3e0ec09a02f1cb4329528d40537e38cd4af5e742
Alex <proskuraaleksandr93@gmail.com>
2020-07-29 10:52:40 +0300

    Initial commit

---
+++ AvengaTestTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AvengaTestTask
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
