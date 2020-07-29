8080e330f3dfc41c63c6bca5a98e6412fd9df564
Alex <proskuraaleksandr93@gmail.com>
2020-07-29 14:33:37 +0300

    Override Equals/GetHashCode methods for Pizza model

--- AvengaTestTask/Models/Pizza.cs
+++ AvengaTestTask/Models/Pizza.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AvengaTestTask.Models
 {
     public class Pizza
     {
         public IList<string> Toppings { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Pizza pizza)
+            {
+                var equals = (Toppings.Count == pizza.Toppings.Count) && !Toppings.Except(pizza.Toppings).Any();
+                return equals;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return 1910748205 + EqualityComparer<IList<string>>.Default.GetHashCode(Toppings);
+        }
     }
 }
