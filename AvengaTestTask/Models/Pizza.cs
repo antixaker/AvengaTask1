607fd638e9a6ffa5bdd155e6e67ea04fd0262637
Alex <proskuraaleksandr93@gmail.com>
2020-07-29 11:33:40 +0300

    Read pizza configurations from Json file

---
+++ AvengaTestTask/Models/Pizza.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AvengaTestTask.Models
+{
+    public class Pizza
+    {
+        public IList<string> Toppings { get; set; }
+    }
+}
--- AvengaTestTask/Program.cs
+++ AvengaTestTask/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using AvengaTestTask.Models;
+using Newtonsoft.Json;
 
 namespace AvengaTestTask
 {
@@ -6,7 +11,23 @@ namespace AvengaTestTask
     {
         public static void Main(string[] args)
         {
+            var pizzas = GetData();
+
             Console.WriteLine("Hello World!");
+        }
+
+        private static IList<Pizza> GetData()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var jsonResource = assembly.GetManifestResourceNames()?[0];
+
+            using (Stream stream = assembly.GetManifestResourceStream(jsonResource))
+            using (StreamReader reader = new StreamReader(stream))
+            using (JsonReader jsonReader = new JsonTextReader(reader))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                return serializer.Deserialize<IList<Pizza>>(jsonReader);
+            }
         }
     }
 }
